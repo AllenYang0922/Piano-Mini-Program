3e5d764458cd78a7f900437cb2b48100dbbecd0d
Allen <1581991552@qq.com>
2026-05-20 10:50:18 +0800

    feat:初始化项目&歌曲清单页面

---
+++ pages/mine/mine.js
@@ -0,0 +1,42 @@
+const defaultAvatarUrl = 'https://mmbiz.qpic.cn/mmbiz/icTdbqWNOwNRna42FI242Lcia07jQodd2FJGIYQfG0LAJGFxM4FbnQP6yfMxBgJ0F3YRqJCJ1aPAK2dQagdusBZg/0'
+
+Page({
+  data: {
+    motto: 'Hello World',
+    userId: '666666',
+    userInfo: {
+      avatarUrl: defaultAvatarUrl,
+      nickName: '',
+    },
+    hasUserInfo: false,
+    canIUseGetUserProfile: wx.canIUse('getUserProfile'),
+    canIUseNicknameComp: wx.canIUse('input.type.nickname'),
+  },
+  onChooseAvatar(e) {
+    const { avatarUrl } = e.detail
+    const { nickName } = this.data.userInfo
+    this.setData({
+      'userInfo.avatarUrl': avatarUrl,
+      hasUserInfo: nickName && avatarUrl && avatarUrl !== defaultAvatarUrl,
+    })
+  },
+  onInputChange(e) {
+    const nickName = e.detail.value
+    const { avatarUrl } = this.data.userInfo
+    this.setData({
+      'userInfo.nickName': nickName,
+      hasUserInfo: nickName && avatarUrl && avatarUrl !== defaultAvatarUrl,
+    })
+  },
+  getUserProfile() {
+    wx.getUserProfile({
+      desc: '展示用户信息',
+      success: (res) => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true,
+        })
+      },
+    })
+  },
+})
---
+++ pages/songs/songs.js
@@ -0,0 +1,11 @@
+Page({
+  data: {
+    songs: [
+      { id: 1, title: 'Always with me' },
+      { id: 2, title: 'Havana' },
+      { id: 3, title: 'K歌之王' },
+      { id: 4, title: 'Yesterday Once More' },
+      { id: 5, title: '一场游戏一场梦' },
+    ],
+  },
+})
